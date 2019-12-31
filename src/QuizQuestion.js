3f3fab8b18ec9c11d93b749a955b0121d8c6ea03
AncutaDaniel <ancutadaniel@gmail.com>
2019-12-31 11:49:12 +0200

    add quiz button

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import QuizQuestionButton from "./QuizQuestionButton";
+
 
 class QuizQuestion extends React.Component {
     render() {
@@ -9,9 +11,11 @@ class QuizQuestion extends React.Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>
-                            {this.props.quiz_question.answer_options[0]}
-                        </li>
+                        <QuizQuestionButton
+                            button_text={
+                                this.props.quiz_question.answer_options[0]
+                            }
+                        />
                     </ul>
                 </section>
             </main>
