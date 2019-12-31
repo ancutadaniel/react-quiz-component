c49f23fa586de41ab1a69f6ea6e683ccfc22a01c
AncutaDaniel <ancutadaniel@gmail.com>
2019-12-31 10:55:33 +0200

    Displaying data from state in the Quiz component

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            quiz_position: 1
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div className="QuizQuestion">
+                    {this.state.quiz_position}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Quiz;
-qq
